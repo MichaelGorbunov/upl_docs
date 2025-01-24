6618329f6e42490163424bb4f6bebf1182e82d5b
Michael Gorbunov <mg0rby@yandex.ru>
2025-01-24 08:39:22 +0300

    swagger add

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,]
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("favicon.ico", RedirectView.as_view(url="/static/img/favicon.ico")),
     path("users/", include("users.urls", namespace="users")),
     path("docs/", include("docs.urls", namespace="docs")),  # маршрут к приложению Привычки
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
