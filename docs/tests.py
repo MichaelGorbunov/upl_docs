619991e2f78d679064d1bfa7d06eb1f88870e71c
Michael Gorbunov <mg0rby@yandex.ru>
2025-01-25 22:23:04 +0300

    увеличил покрытие тестов

--- docs/tests.py
+++ docs/tests.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 from docs.models import Upload
 from django.core.files.uploadedfile import SimpleUploadedFile
 from users.models import CustomUser
@@ -9,9 +9,58 @@ from users.models import CustomUser
 class FileUploadTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create_user(email="user1@habits.ru", username="user1", password="12345678")
+        self.other_user = CustomUser.objects.create_user(username='other_user', email='2@test.com',
+                                                         password='password123')
+
+        self.uploaded_file = Upload.objects.create(
+            owner=self.user,
+            file='dummy/path/test.txt'
+            # Укажите реальный путь к файлу на диске (для теста вы можете использовать заглушку)
+        )
+
+        # Выводим ID загруженного файла для отладки
+        print(f"Uploaded file ID: {self.uploaded_file.id}")
+
+        # Инициализация APIRequestFactory
+        self.factory = APIRequestFactory()
+
+    def test_2file_list(self):
+        '''Тест списка'''
+
         self.client.force_authenticate(user=self.user)  # Аутентификация пользователя
-    def test_file_upload(self):
+
+        # Отправьте POST-запрос к вашему API с файлом
+        response = self.client.get('/docs/list/')
+
+        # Проверьте статус ответа
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_3file_list(self):
+        '''Тест удаления не владельцем'''
+
+        self.client.force_authenticate(user=self.other_user)  # Аутентификация пользователя
+
+        # Отправьте POST-запрос к вашему API с файлом
+        response = self.client.delete('/docs/delete/2/')
+
+        # Проверьте статус ответа
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_4file_list(self):
+        '''Тест удаления владельцем'''
+
+        self.client.force_authenticate(user=self.user)  # Аутентификация пользователя
+
+        # Отправьте POST-запрос к вашему API с файлом
+        response = self.client.delete('/docs/delete/3/')
+
+        # Проверьте статус ответа
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_5file_upload(self):
         '''Тест загрузки файла'''
+
+        self.client.force_authenticate(user=self.user)  # Аутентификация пользователя
         # Создайте временный файл
         file_content = b'This is a test file.'
         uploaded_file = SimpleUploadedFile("testfile.txt", file_content)
