adcb593eae923248ab86348173a435ae6f51444f
Michael Gorbunov <mg0rby@yandex.ru>
2025-01-29 16:52:06 +0300

    ALLOWED_HOSTS = ['web', 'localhost', ]

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['s120601.foxcdn.net', ]
+# ALLOWED_HOSTS = ['s120601.foxcdn.net', ]
+ALLOWED_HOSTS = ['web', 'localhost', ]
 # ALLOWED_HOSTS = ['app', 'nginx', ]
 # Application definition
 
